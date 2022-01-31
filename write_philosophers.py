547022415685747ea3a7017a60462bdc6a312a0e
Caleb Fingel <caleb_fingel1@baylor.edu>
2022-01-31 15:44:21 -0600

    jan31

--- write_philosophers.py
+++ write_philosophers.py
@@ -1,11 +1,19 @@
 def main():
     outfile = open("philosophers.txt",'w')
-    outfile.write("John Locker" + '\n')
-    outfile.write("David Humer" + '\n')
+    outfile.write("John Locke" + '\n')
+    outfile.write("David Hume" + '\n')
     outfile.write("Edmund Burke" + '\n')
 
     outfile.close()
 
+def add_my_name():
+    outfile =open("philosophers.txt",'a')
+    outfile.write("Caleb Fingel\n")
+
+    outfile.close()
+
+
 main()
+add_my_name()
 
 
