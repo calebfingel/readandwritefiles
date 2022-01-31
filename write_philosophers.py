6d72d349ba90cdbcb0b493dd8227235bbfabdfed
Caleb Fingel <caleb_fingel1@baylor.edu>
2022-01-31 15:07:18 -0600

    pphilospher names

---
+++ write_philosophers.py
@@ -0,0 +1,11 @@
+def main():
+    outfile = open("philosophers.txt",'w')
+    outfile.write("John Locker" + '\n')
+    outfile.write("David Humer" + '\n')
+    outfile.write("Edmund Burke" + '\n')
+
+    outfile.close()
+
+main()
+
+
